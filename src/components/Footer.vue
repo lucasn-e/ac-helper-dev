24c471bfd88c2562ffc5721552fba211e8a5e43f
x912 <xxsns2643@gmail.com>
2020-03-25 08:20:18 +0100

    fixed credits

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    App made by Nnay (@acnnay on instagram) - data taken from gamespot. sadly their list was ugly and stupid
+    App made by Nnay (@acnnay on instagram || Nnay#8093 on discord) - data used from gamewith.net - gamespot data was missing entries
     <br />I am very lazy so please be patient or send me messages to encourage me to continue
   </div>
 </template>
