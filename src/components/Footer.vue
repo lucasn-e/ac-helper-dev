b075bd502560386eaf647eb2203d5cd0cec82c4d
x912 <xxsns2643@gmail.com>
2020-04-04 13:51:03 +0200

    added discord link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="footer">
     App made by Nnay (@acnnay on instagram || Nnay#8093 on discord) - data used from gamewith.net - gamespot data was missing entries
-    <br />I am very lazy so please be patient or send me messages to encourage me to continue
+    <br />I am very lazy so please be patient or send me messages to encourage me to continue.
+    <a
+      href="https://discord.gg/G8mqHk"
+    >Join the Discord!</a>
   </div>
 </template>
