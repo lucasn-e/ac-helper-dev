638da187a34705a1a2e2e4f91cf540f25a0d9e4f
x912 <xxsns2643@gmail.com>
2020-04-04 23:55:24 +0200

    fix a little ugly in the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    App made by Nnay (@acnnay on instagram || Nnay#8093 on discord) - data used from gamewith.net - gamespot data was missing entries
+    made by Nnay (@acnnay on instagram || Nnay#8093 on discord) - data used from gamewith.net
     <br />I am very lazy so please be patient or send me messages to encourage me to continue.
     <a
       href="https://discord.gg/G8mqHk"
