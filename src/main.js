465f093bd44b6ee32487a7f92331ac4af49f9050
x912 <xxsns2643@gmail.com>
2020-04-05 16:35:06 +0200

    maybe pwa

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store/index.js';
+
 // eslint-disable-next-line
 import styles from './scss/app.scss';
+
+import './registerServiceWorker'
 
 const prod = process.env.NODE_ENV === 'production'
 const shouldSW = 'serviceWorker' in navigator && prod
