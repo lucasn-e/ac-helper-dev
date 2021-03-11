c35e9624faf1c5dfcd4b3534c0638c09aa90fb9b
Lucas <xxsns2643@gmail.com>
2021-03-11 07:58:35 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="footer">
-    made by Nnay (@acnnay on instagram || Nnay#8093 on discord) - data used from gamewith.net
     <br />I am very lazy so please be patient or send me messages to encourage me to continue.
-    <a
-      href="https://discord.gg/84EsCJz"
-    >Join the Discord!</a>
     Icons made by
     <a
       href="https://www.flaticon.com/authors/dinosoftlabs"
